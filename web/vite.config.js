b87389447cce9cb70f13a13dc3ac8b688d14f611
Mansur <yusupov7274@gmail.com>
2026-01-25 21:19:04 +0300

    V2

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/tg-finance-miniapp/',
   server: {
     host: 'localhost',
     port: 3000,
@@ -10,5 +12,9 @@ export default defineConfig({
       'web.xn--b1akc1f.store',
       'app.xn--b1akc1f.store',
     ],
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
   },
 })
