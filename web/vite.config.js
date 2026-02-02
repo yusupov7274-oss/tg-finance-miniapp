243cd7c3c61d71e28c81b8544bd94c89ea2e84e0
Mansur <yusupov7274@gmail.com>
2026-02-02 23:05:21 +0300

    изменения по белой странице

--- web/vite.config.js
+++ web/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/tg-finance-miniapp/',
+  base: '/',
   server: {
     host: 'localhost',
     port: 3000,
